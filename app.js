9f7181638f57e364ed9686e84f4fa0bca8f95970
Marcin Owerczuk <marcin.owerczuk@deltatre.com>
2019-08-21 17:43:40 +0100

    mongoose, favicon

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const favicon = require('serve-favicon');
+
+require('./app_server/models/db');
 
 const indexRouter = require('./app_server/routes/index');
 
@@ -17,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 
 app.use('/', indexRouter);
 
