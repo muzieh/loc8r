57e5c1c9bb42cfa9ec2354f72e8b81b813693406
Marcin Owerczuk <muzieh@gmail.com>
2019-10-07 22:17:07 +0100

    mongo calls for locations

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -2,12 +2,44 @@ const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
 const locationsListByDistance = (req, res) => { 
-    res
-        .status(200)
-        .json({a:10, b:20});
+    Loc
+        .find()
+        .exec((err, locations) => {
+            res
+                .status(200)
+                .json({
+                    "status": "success",
+                    "locations": locations
+                });
+        });
 };
-const locationsCreate = (req, res) => { };
-const locationsReadOne = (req, res) => { };
+const locationsCreate = (req, res) => {
+    res
+        .status(401)
+        .json({"status": "success"})
+};
+
+const locationsReadOne = (req, res) => {
+    Loc
+        .findById(req.params.locationid)
+        .exec((err, location) => {
+            if(!location) {
+                return res
+                        .status(404)
+                        .json({"message": "location not found"})
+            } else if (err) {
+                return res
+                        .status(404)
+                        .json(err)
+            } 
+            
+            res
+                .status(200)
+                .json(location);
+            
+        });
+};
+
 const locationsUpdateOne = (req, res) => { };
 const locationsDeleteOne = (req, res) => { };
 
