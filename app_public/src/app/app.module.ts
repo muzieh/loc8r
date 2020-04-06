a5ecd79b5dc6d8fc395f0294ff2572360bc7ff3f
Marcin Owerczuk <muzieh@gmail.com>
2020-04-06 23:25:43 +0200

    Angular SPA routing, framework, about

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from "@angular/common/http";
+import { RouterModule} from "@angular/router";
 import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe';
-
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([{
+      path: "",
+      component: HomeListComponent
+    }, {
+      path: "about",
+      component: AboutComponent
+    }])
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
