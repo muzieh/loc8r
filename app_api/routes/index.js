b9b8bddfdb5278575db802c406c5a4797831f371
Marcin Owerczuk <marcin.owerczuk@deltatre.com>
2020-04-13 13:08:18 +0200

    JWT generation, login, register api

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlReviews = require('../controllers/reviews');
+const ctrlAuth = require('../controllers/authentication');
+
+
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 
 router
     .route('/locations')
